4a205a0e20531018a817e413f47d7c1df6f83153
scrpgil <scraplbuild@gmail.com>
2019-03-02 14:46:19 +0900

    remove constructor

--- functions/src/lib/storage.ts
+++ functions/src/lib/storage.ts
@@ -1,7 +1,4 @@
 export class StorageLib {
-  constructor() {
-    console.log("new storage lib");
-  }
   upload(storage: any, blob: any, filename: any, path: any) {
     return new Promise(resolve => {
       const new_file = storage.bucket().file(path + "/" + filename);
